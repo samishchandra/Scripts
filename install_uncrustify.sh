7cad26a5333eb10412013270e92093474032339d
Samish Chandra Kolli <samishchandra@gmail.com>
2019-11-14 20:33:12 -0800

    Update script with echo statements

--- install_uncrustify.sh
+++ install_uncrustify.sh
@@ -6,31 +6,39 @@ set -e
 filename="FB XC Extensions"
 
 # delete old formula
+echo "Removing existing Uncrustify formula"
 brew formula samish_uncrustify | xargs rm -rf
 
 # brew install samish_uncrustify
+echo "Installing Uncrustify"
 brew reinstall -f https://raw.githubusercontent.com/samishchandra/homebrew/master/Formula/samish_uncrustify.rb
 
 # download config file
+echo "Updating Uncrustify config file"
 curl -fSL https://raw.githubusercontent.com/samishchandra/uncrustify/master/archive/uncrustify.cfg -o $HOME/uncrustify.cfg
 
 # download Xcode extension
-curl -fsSL https://www.dropbox.com/s/4iys375hd3wpwre/FB%20XC%20Extensions.zip -o "${filename}.zip"
+echo "Downloading Xcode extension"
+curl -fsSL https://www.dropbox.com/s/jfxiugx3v578rz5/FB%20XC%20Extensions.zip -o "${filename}.zip"
 
 # unzip Xcode extension
+echo "Unzip Xcode extension"
 unzip "${filename}.zip"
 
 # Move app to applications folder and open
+echo "Copy Xcode extension"
 cp -Rf "${filename}/${filename}.app" /Applications/
 open "/Applications/${filename}.app"
 
 # update to latest config file if needed
+echo "Update config file for Xcode extension"
 extension_app_folder_path=$HOME/Library/Containers/FB.FB-XC-Extensions-Host.FB-Uncrustify/Data
 if [ -d $app_folder_path/uncrustify ]; then
   curl -fSL https://raw.githubusercontent.com/samishchandra/uncrustify/master/archive/uncrustify.cfg -o $extension_app_folder_path/uncrustify/uncrustify.cfg
 fi
 
 # cleanup
+echo "Cleanup"
 rm -rf "${filename}"
 rm -rf "${filename}.zip"
 rm -rf "${filename}.app"
