e9d3cb81ef4666f1ab96ce1fb3c710bbd2023d14
Samish Chandra Kolli <samishchandra@gmail.com>
2019-11-14 20:33:12 -0800

    Update script with correct config file path

--- install_uncrustify.sh
+++ install_uncrustify.sh
@@ -15,23 +15,23 @@ brew reinstall -f https://raw.githubusercontent.com/samishchandra/homebrew/maste
 
 # download config file
 echo "Updating Uncrustify config file"
-curl -fSL https://raw.githubusercontent.com/samishchandra/uncrustify/master/archive/uncrustify.cfg -o $HOME/uncrustify.cfg
+curl -fSL https://raw.githubusercontent.com/samishchandra/homebrew/master/archive/uncrustify/uncrustify.cfg -o $HOME/uncrustify.cfg
 
 # download Xcode extension
 echo "Downloading Xcode extension"
 curl -fsSL https://www.dropbox.com/s/jfxiugx3v578rz5/FB%20XC%20Extensions.zip -o "${filename}.zip"
 
 # unzip Xcode extension
-echo "Unzip Xcode extension"
+echo "Unzipping Xcode extension"
 unzip "${filename}.zip"
 
 # Move app to applications folder and open
-echo "Copy Xcode extension"
+echo "Copying Xcode extension to Applications folder"
 cp -Rf "${filename}/${filename}.app" /Applications/
 open "/Applications/${filename}.app"
 
 # update to latest config file if needed
-echo "Update config file for Xcode extension"
+echo "Updating config file for Xcode extension"
 extension_app_folder_path=$HOME/Library/Containers/FB.FB-XC-Extensions-Host.FB-Uncrustify/Data
 if [ -d $app_folder_path/uncrustify ]; then
   curl -fSL https://raw.githubusercontent.com/samishchandra/uncrustify/master/archive/uncrustify.cfg -o $extension_app_folder_path/uncrustify/uncrustify.cfg
